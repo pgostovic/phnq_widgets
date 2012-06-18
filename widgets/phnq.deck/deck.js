2812d7ba7f31bbfbe9b8b858b2efd9f7f29752df
Patrick Gostovic <pg@phranq.com>
2012-06-18 10:23:22 -0400

    Deck now supports show by key.

--- widgets/phnq.deck/deck.js
+++ widgets/phnq.deck/deck.js
@@ -1,12 +1,28 @@
 var widget =
 {
+    order: -1,
+
     ready: function($$)
     {
-        this.show(0);
+        this.keys = this.params.keys;
+
+        if(this.keys)
+        {
+            this.keyLookup = {};
+            for(var i=0; i<this.keys.length; i++)
+            {
+                this.keyLookup[this.keys[i]] = i;
+            }
+        }
+
+        this.show(-1);
     },
 
     show: function(idx)
     {
+        if(typeof(idx) == "string" && this.keys)
+            idx = this.keyLookup[idx];
+
         this.get$$()("> *").each(function(i)
         {
             if(i == idx)
