66bea979f2fe6d3b6cfd6859dc7c4efe0b334982
Patrick Gostovic <pg@phranq.com>
2012-10-19 15:21:42 -0400

    All travis'd up.

---
+++ test/phnq_widgets_test.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+var phnq_widgets = require("../server/phnq_widgets");
+
+describe("phnq_widgets", function()
+{
+	describe("stuff", function()
+	{
+		it("should extend do stuff", function()
+		{
+			assert.equal(1, 2);
+		});
+	});
+});
