c795d30cac57a8b961cd9c63eebfb735a54a961b
Patrick Gostovic <pg@phranq.com>
2012-10-31 16:59:40 -0400

    Added a bit of control.

--- widgets/phnq.accordion/accordion.js
+++ widgets/phnq.accordion/accordion.js
@@ -2,10 +2,17 @@
 {
 	var animTime = 200;
 
-	$.fn.accordion = function()
+	$.fn.accordion = function(options)
 	{
-		var accordElmnt = this;
+		options = options || {};
+		if(options.expand)
+			accordianExpand(options.expand);
+		else
+			accordionInit(this);
+	};
 
+	var accordionInit = function(accordElmnt)
+	{
 		// hide all to begin
 		$("h3", accordElmnt).each(function()
 		{
@@ -14,21 +21,26 @@
 
 		$("h3", accordElmnt).click(function()
 		{
-			var clickedH3 = this;
-
-			$(clickedH3).addClass("selected");
-			$(clickedH3).next().slideDown(animTime);
-
-			$("h3", accordElmnt).each(function()
-			{
-				if(this != clickedH3)
-				{
-					$(this).removeClass("selected");
-					$(this).next().slideUp(animTime);
-				}
-			});
+			accordianExpand(this);
 		});
 
 		$(accordElmnt).addClass("phnq.accordion");
+	};
+
+	var accordianExpand = function(elmnt)
+	{
+		log.debug("accordianExpand: ", elmnt);
+
+		$(elmnt).addClass("selected");
+		$(elmnt).next().slideDown(animTime);
+
+		$(elmnt).closest(".phnq\\.accordion").find("h3").each(function()
+		{
+			if(this != elmnt)
+			{
+				$(this).removeClass("selected");
+				$(this).next().slideUp(animTime);
+			}
+		});
 	};
 })(jQuery);
